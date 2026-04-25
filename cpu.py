58407931503162844d3454cadb43528e2bb801aa
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-04-25 20:16:10 -0300

    Ajuste de comentários em cpu.py

--- cpu.py
+++ cpu.py
@@ -3,8 +3,8 @@ class CPU:
         self.auxs = [0] * 4             ## Array de auxiliares
         self.mem = [0] * 256            ## Memória principal
         self.instrucoes = {}            ## Dicionário de instruções
-        self.entrada = 0                ##
-        self.flag_entrada = False       ##
+        self.entrada = 0                ## Valor de entrada fornecido externamente (I/O)
+        self.flag_entrada = False       ## Indica se há um valor de entrada aguardando leitura
         self.ac = 0                     ## Acumulador
         self.z = 0                      ## 0 - resultado nulo | 1 - resultado positivo
         self.p = 0                      ## 0 - resultado negativo | 1 - resultado positivo
