9ad0b0c68f3469db8a622cd0263ca223463d9b5f
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-04-25 17:50:42 -0300

    Inicio da montagem do esqueleto do código refatorado

---
+++ leitor.py
@@ -0,0 +1,8 @@
+class Parser:
+    def __init__(self, caminho_arquivo: str):
+        self.caminho_arquivo = caminho_arquivo
+        self.instrucoes = {}
+
+    def parsear(self):
+        pass
+        return self.instrucoes
