6c3d2129b01953949f22705c6c0859ff29d2dc8d
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-05-12 21:34:23 -0300

    Continuação do leitor.py

--- leitor.py
+++ leitor.py
@@ -4,12 +4,25 @@ class Parser:
         self.caminho_arquivo = caminho_arquivo      # Armazena o caminho do arquivo
         self.instrucoes = {}                        # Dicionário para armazenar as instruções
 
-    def parsear(self):                  ## Função para realizar o parsing do arquivo
+    def parsear(self):  ## Função para realizar o parsing do arquivo
 
-        conteudo = open(self.caminho_arquivo, 'r')  # abre o arquivo no caminho no modo read
-        conteudo.read()                             # tranforma todo o conteúdo do arquivo em uma string
+        with open(self.caminho_arquivo, 'r') as arquivo:    # abre o arquivo no caminho no modo read. conteudo é objeto
+            conteudo = arquivo.read()                       # tranforma o conteúdo do arquivo em uma string. conteudo passa a ser string
+            linhas = conteudo.splitlines()                  # trasfomar a string em uma lista de strings, cada uma representando uma linha do arquivo
 
+            linhas_validas = []  # lista de linhas válidas do codigo
 
+            for linha in linhas:                    # para cada linha em linhas
+                if '|' in linha:                    # se houver '|'
+                    linhas_validas.append(linha)    # adiciona em linhas_validas
 
+            comandos = []     # lista com instruções válidas
+
+            for linha in linhas_validas:    # para cada linha em linhas_validas
+                partes = linha.split('|')   # .split() retorna uma lista
+
+                rotulo = partes[1].strip().upper()      # transforma o elemento 0 da lista em rotulo, deixa em maiúsculo e retira os espaços
+                mnemonico = partes[0].strip().upper()   # transforma o elemento 1 da lista em menemônico, deixa em maiúsculo e retira os espaços
+                operando = partes[2].strip().upper()    # transforma o elemento 2 da lista em operando, deixa em maiúsculo e retira os espaços
 
         return self.instrucoes
