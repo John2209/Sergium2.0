65e697a5affb289070b646c7c04d31451ab3631b
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-05-17 00:34:01 -0300

    Ajustes do main.py para testes

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from app import App
 from cpu import CPU
 from leitor import Parser
 
-if __name__ == "__main__":              ## Ponto de entrada do programa
+if __name__ == "__main__":
+    minha_cpu = CPU()
 
-        minha_cpu = CPU()               # Cria instância da CPU
-        meu_parser = Parser("")         # Cria instância do parser
-        meu_app = App(minha_cpu)        # Cria instância da aplicação
+    parser = Parser(r"C:\dev\mSergium\codigos\pTestCompleto.txt")
+    instrucoes = parser.parsear()
 
-        meu_app.mainloop()              # Inicia o loop principal da interface
+    minha_cpu.carregar_programa(instrucoes)
+
+    acabou = False
+
+    while not acabou:
+        acabou = minha_cpu.executar_instrucao()
+        print("AC:", minha_cpu.ac)
+        print("AUX:", minha_cpu.auxs)
+        print("Z:", minha_cpu.z)
+        print("P:", minha_cpu.p)
+        print("PC:", minha_cpu.pc)
+        print("---")
