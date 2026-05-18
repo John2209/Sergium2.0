2f280db85dae64480e923786d06519841ac286d1
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-05-18 17:08:41 -0300

    test: definição da entrada da cpu

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from leitor import Parser
 if __name__ == "__main__":
     minha_cpu = CPU()
 
-    parser = Parser(r"C:\dev\mSergium\codigos\pTestCompleto.txt")
+    parser = Parser(r"C:\dev\mSergium\codigos\pTesteIO.txt")
     instrucoes = parser.parsear()
 
     minha_cpu.carregar_programa(instrucoes)
+    minha_cpu.entrada = 5
 
     acabou = False
 
@@ -18,4 +19,5 @@ if __name__ == "__main__":
         print("Z:", minha_cpu.z)
         print("P:", minha_cpu.p)
         print("PC:", minha_cpu.pc)
+        print("SAÍDA:", minha_cpu.saida)
         print("---")
