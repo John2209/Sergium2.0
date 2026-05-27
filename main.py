dedb952aadbca9d6c8143ca5aa040c9889d474b9
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-05-26 22:01:37 -0300

    test: novo main() para testes

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from cpu import CPU
-from leitor import Parser
+from core.cpu import CPU
+from core.parser import Parser
+from app import App
 
 if __name__ == "__main__":
     minha_cpu = CPU()
-
-    parser = Parser(r"C:\dev\mSergium\codigos\pTesteIO.txt")
-    instrucoes = parser.parsear()
-
-    minha_cpu.carregar_programa(instrucoes)
-    minha_cpu.entrada = 5
-
-    acabou = False
-
-    while not acabou:
-        acabou = minha_cpu.executar_instrucao()
-        print("AC:", minha_cpu.ac)
-        print("AUX:", minha_cpu.auxs)
-        print("Z:", minha_cpu.z)
-        print("P:", minha_cpu.p)
-        print("PC:", minha_cpu.pc)
-        print("SAÍDA:", minha_cpu.saida)
-        print("---")
+    app = App(minha_cpu)
+    app.mainloop()
