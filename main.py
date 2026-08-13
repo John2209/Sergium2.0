2ca0b16f0e5359b130dc48394188c7f0bbb96b53
João Batistella da Costa <joaobatistelladacosta@gmail.com>
2026-08-12 21:30:12 -0300

    chore(legacy): save do Sergium 1.0

--- main.py
+++
@@ -1,7 +0,0 @@
-from core.cpu import CPU
-from ui.app import App
-
-if __name__ == "__main__":
-    minha_cpu = CPU()
-    app = App(minha_cpu)
-    app.mainloop()
--- ui/__init__.py
+++
@@ -1,6 +0,0 @@
-# pacote com os componentes visuais
-from .registers_panel import RegistersPanel      # exporta o painel de registradores
-from .editor_painel import EditorPanel            # exporta o painel do editor
-from .instructions_panel import InstructionsPanel  # exporta o painel de instruções
-from .memory_panel import MemoryPanel            # exporta o painel de memória
-from .terminal_panel import TerminalPanel        # exporta o painel do terminal
